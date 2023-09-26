0d1811259150a53ceca321bf1d511982c0e35105
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-09-26 15:14:25 +0300

    added scroll

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,39 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import styles from './PostList.module.css'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import InfiniteScroll from "react-infinite-scroll-component";
+import styles from "./PostList.module.css";
 
-const fetchPosts = async () => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  return response.data;
+const fetchPosts = async (page) => {
+  try {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
 };
 
 const PostList = () => {
-  const { data: posts, isLoading } = useQuery("posts", fetchPosts);
+  const [page, setPage] = useState(1);
+  const {
+    data: posts,
+    isLoading,
+    fetchMore,
+  } = useQuery(["posts", page], () => fetchPosts(page), {
+    keepPreviousData: true,
+    staleTime: 60000,
+  });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+          document.documentElement.offsetHeight &&
+        !isLoading
+      ) {
+        fetchMore();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [fetchMore, isLoading]);
 
   return (
     <div className={styles.container}>
-      <div className={styles.post_list}>
-        {posts.map((post) => (
-          <div className={styles.post} key={post.id}>
-            <h2>{post.title}</h2>
-            <p>
-              {post.body.length > 100
-                ? `${post.body.slice(0, 100)}...`
-                : post.body}
-            </p>
-            <Link to={`/post/${post.id}`}>Просмотр</Link>
-          </div>
-        ))}
-      </div>
+      <InfiniteScroll
+        dataLength={posts ? posts.length : 0}
+        next={() => setPage((prevPage) => prevPage + 1)}
+        hasMore={Boolean(posts && posts.length > 0)}
+        loader={<h4>Loading...</h4>}
+      >
+        <div className={styles.post_list}>
+          {posts &&
+            posts.map((post) => (
+              <div className={styles.post} key={post.id}>
+                <h2>{post.title}</h2>
+                <p>
+                  {post.body.length > 100
+                    ? `${post.body.slice(0, 100)}...`
+                    : post.body}
+                </p>
+                <Link to={`/post/${post.id}`}>Просмотр</Link>
+              </div>
+            ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
